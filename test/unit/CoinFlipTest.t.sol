0d0bee071d03e5e0744658276784af84d7ef7b74
Miguel Martinez <miguerdev@gmail.com>
2023-08-01 00:39:03 -0400

    Update CoinFlip test

--- test/unit/CoinFlipTest.t.sol
+++ test/unit/CoinFlipTest.t.sol
@@ -7,27 +7,23 @@ import {CoinFlip} from "../../src/CoinFlip.sol";
 contract CoinFlipTest is Test {
     CoinFlip coinFlip;
 
-    address TEST_USER = makeAddr("user");
-    uint256 constant STARTTING_BALANCE = 100 ether;
     uint256 constant FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     function setUp() external {
-        vm.deal(TEST_USER, STARTTING_BALANCE);
-        vm.prank(TEST_USER);
         coinFlip = new CoinFlip();
     }
 
     function testGuessFlip() public {
-        uint256 flipValue = getCoinFlip();
-        console.log(flipValue);
-        bool side = flipValue == 1 ? true : false;
-        coinFlip.flip(side);
-
+        coinFlip.flip(getCoinFlip());
         assert(coinFlip.consecutiveWins() > 0);
     }
 
-    function getCoinFlip() internal view returns (uint256) {
-        uint256 blockValue = uint256(blockhash(block.number - 1));
-        return blockValue / FACTOR;
+    function getCoinFlip() internal view returns (bool) {
+        uint256 blockValue = (uint256(blockhash(block.number - 1))) / FACTOR;
+        console.log(uint256(blockhash(block.number - 1)));
+        console.log((uint256(blockhash(block.number - 1))) / FACTOR);
+        bool side = blockValue == 1 ? true : false;
+        console.log(side);
+        return side;
     }
 }
